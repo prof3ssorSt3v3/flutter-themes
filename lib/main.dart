0e2ba4061dda29771e00ebe6cc4ce5d288c7ac3c
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2022-04-01 11:44:19 -0400

    initial files and themes

---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import './utils/my_theme.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: MyTheme.buildDark(),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: const Icon(Icons.back_hand),
+          title: const Text('Themed App'), //gets font from AppBarTheme
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Some Title',
+                        style: Theme.of(context).textTheme.headline3,
+                        // we can use Theme.of(context) to target props from Theme
+                      ),
+                      Text(
+                        'Some SubTitle',
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                      Text(
+                        'lots and lots and lots of body text',
+                        style: Theme.of(context).textTheme.bodyText2,
+                      ),
+                      Icon(Icons.radar), //uses IconThemeData
+                    ],
+                  ),
+                ),
+              ),
+              const ListTile(
+                title: Text('ListTile Title'), //uses textTheme.subTitle1
+                subtitle: Text('ListTile Subtitle'), //uses textTheme.bodyText2
+                leading: Icon(Icons.card_giftcard),
+              ),
+              ElevatedButton(
+                child: const Text('Click This'),
+                style: ElevatedButton.styleFrom(
+                  primary: Theme.of(context).primaryColor,
+                  onPrimary: Colors.black87,
+                  // Theme.of(context).colorScheme.primary, //from colorScheme
+                ),
+                onPressed: () {
+                  //click the button
+                  //colors from ElevatedButton.styleFrom()
+                },
+              ),
+              ElevatedButton(
+                child: const Text('Click again'),
+                //using the style from the theme
+                onPressed: () {
+                  //click the button
+                  //colors from ElevatedButtonTheme
+                },
+              ),
+              TextButton(
+                onPressed: () {
+                  //click the button
+                  //color from textButtonTheme
+                },
+                child: Text('TextButton'),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (() {}),
+          child: const Icon(Icons.add),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: 1,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.abc),
+                label: 'ABC',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.watch),
+                label: 'Watch',
+              ),
+            ],
+            onTap: (int index) {
+              //change the active bar item
+            }),
+      ),
+    );
+  }
+}
---
+++ lib/utils/my_theme.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+
+class MyTheme {
+  MyTheme();
+
+  static ThemeData buildDark() {
+    //final base = ThemeData.dark();
+
+    //ThemeData.from( colorScheme: , textTheme: )
+    final ThemeData base = ThemeData.from(
+      colorScheme: const ColorScheme(
+        //base colour scheme that can be overridden for widgets
+        primary: Colors.yellow,
+        onPrimary: Colors.purple,
+        secondary: Colors.blueGrey,
+        onSecondary: Colors.lime,
+        tertiary: Colors.amber,
+        onTertiary: Colors.lime,
+
+        primaryContainer: Colors.white,
+        onPrimaryContainer: Colors.black,
+        secondaryContainer: Colors.blueAccent,
+        onSecondaryContainer: Colors.black,
+        tertiaryContainer: Colors.blueAccent,
+        onTertiaryContainer: Colors.black,
+
+        background: Colors.indigoAccent,
+        onBackground: Color(0xFF003366),
+        surface: Colors.lightGreen,
+        onSurface: Colors.grey,
+        error: Colors.pink,
+        onError: Colors.black87,
+
+        brightness: Brightness.light, // will switch text between dark/light
+        //if colorScheme is light the text will be dark
+      ),
+      textTheme: const TextTheme(
+        //base texttheme that can be overridden by widgets
+        headline1: TextStyle(
+          // letterSpacing: ,
+          // fontFamily: ,
+          fontSize: 60,
+          fontWeight: FontWeight.w700,
+        ),
+        headline2: TextStyle(
+          fontSize: 48,
+          fontWeight: FontWeight.w500,
+        ),
+        headline3: TextStyle(
+          fontSize: 48,
+          fontWeight: FontWeight.w300,
+        ),
+        headline4: TextStyle(
+          fontSize: 36,
+          fontWeight: FontWeight.w300,
+        ),
+        headline5: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.w500,
+        ),
+        headline6: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w500,
+        ),
+        bodyText1: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.w300,
+        ),
+        bodyText2: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w300,
+        ),
+        subtitle1: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.w300,
+        ),
+        subtitle2: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w300,
+        ),
+        button: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+    );
+
+    //then build on top of the colorScheme and textTheme
+    //to style specific widgets
+    ThemeData dark = base.copyWith(
+      //colours set in here will override the ColorScheme
+      scaffoldBackgroundColor: Colors.yellow[900],
+      shadowColor: Color(0x9900cc66),
+
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.lightGreen,
+        foregroundColor: Colors.yellowAccent,
+        titleTextStyle: TextStyle(
+          fontFamily: 'SendFlowers',
+          fontSize: 36,
+        ),
+        iconTheme: IconThemeData(color: Colors.deepOrange),
+        //this will override the iconThemeData
+      ),
+
+      iconTheme: const IconThemeData(
+        //defaults for icons unless overridden
+        color: Colors.orange,
+        size: 36,
+      ),
+
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: Colors.indigo[200],
+        foregroundColor: Colors.black54, //for the icon
+        elevation: 20, //for all FABs
+      ),
+
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.amber),
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+          elevation: MaterialStateProperty.all(10), //for all ElevatedButtons
+        ),
+      ),
+
+      textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.purple),
+        ),
+      ),
+
+      cardTheme: const CardTheme(
+        color: Color(0xFF339966), //background of card
+        elevation: 12, //shadow distance, z-index for all cards
+        //to change the rounding of the corners use shape
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(24),
+          ),
+        ),
+        margin: EdgeInsets.symmetric(
+          vertical: 16.0,
+          horizontal: 32.0,
+        ),
+      ),
+
+      listTileTheme: const ListTileThemeData(
+        tileColor: Colors.amber,
+        // textColor: Colors.black, //sets both title and subtitle
+        style: ListTileStyle.list,
+        //ListTileStyle.list means use subtitle1 for the title
+        //ListTileStyle.drawer means use bodyText1 for the title
+        dense: false,
+        iconColor: Colors.red,
+      ),
+
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        backgroundColor: Colors.amber[200],
+        elevation: 16,
+        selectedIconTheme: const IconThemeData(color: Colors.red),
+        unselectedIconTheme: const IconThemeData(color: Colors.lightBlue),
+      ),
+    );
+
+    return dark;
+  }
+
+  // ThemeData light = ThemeData.from(
+  //   colorScheme: ColorScheme.fromSwatch(),
+  // );
+}
